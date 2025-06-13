0ed541425533d2f50798865b9c751d187da66662
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-06-12 22:55:03 -0500

    Create reorder_ll.py

---
+++ reorder_ll.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import collections
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        queue = deque()
+        current = head
+        while current!=None:
+            queue.append(current)
+            current=current.next
+        head = queue.popleft()
+        use_tail = True
+        dummy = head
+        while queue:
+            if use_tail:
+                element = queue.pop()
+            else:
+                element = queue.popleft()
+
+            dummy.next = element
+            dummy = element
+            use_tail = not use_tail
+        dummy.next = None
