abdf62150f290832aabba17601cd8a481cd175f7
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-06-15 23:18:53 -0500

    Create word_break.py

---
+++ word_break.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False for _ in range(len(s)+1)]
+        wordset = set(wordDict)
+        dp[0]=True
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordset:
+                    dp[i] = True
+                    break
+        if dp[-1] == True:
+            return True
+        return False
+
