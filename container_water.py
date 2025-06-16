e1fe72671c9f26bd822aeaa6effae9bf2d33f43e
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-06-15 22:55:24 -0500

    Create container_water.py

---
+++ container_water.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        ptr1,ptr2 = 0, len(height)-1
+        max_vol = 0
+        while ptr1<ptr2:
+            max_vol = max(max_vol,(ptr2-ptr1)*min(height[ptr1],height[ptr2]))
+            if height[ptr1]<height[ptr2]:
+                ptr1+=1
+            else:
+                ptr2-=1
+        return max_vol
