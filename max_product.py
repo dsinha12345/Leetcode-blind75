8d7759fc2c96989f367abaf1cf099e46592d6d6e
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-06-16 12:41:26 -0500

    Create max_product.py

---
+++ max_product.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        maxp = minp = res = nums[0]
+        for i in nums[1:]:
+            if i<0:
+                maxp,minp = minp,maxp
+            maxp = max(i, i*maxp)
+            minp = min(i, i*minp)
+            res = max(maxp,res)
+        return res
+
+
