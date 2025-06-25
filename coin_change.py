005bfb84c7a1b42ea4347c2a8f4055518e8a9d49
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-06-24 21:30:24 -0500

    Create coin_change.py

---
+++ coin_change.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount ==0:
+            return 0
+        dp = [math.inf for _ in range(amount+1)]
+        dp[0] = 0
+
+        for i in range(len(dp)):
+            for num in coins:
+                if num<=i:
+                    left = i - num
+                    dp[i] = min(dp[left]+1,dp[i])
+        if dp[-1] == math.inf:
+            return -1
+        else:
+            return dp[-1]
