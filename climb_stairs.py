574ee1a695b7e9e7459c155029390247d1242c02
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-06-24 21:38:53 -0500

    Update climb_stairs.py

--- climb_stairs.py
+++ climb_stairs.py
@@ -1,6 +1,9 @@
 class Solution:
+    def __init__(self):
+        self.memo = {0: 1, 1: 1}
+    
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n==1:
-            return 1
-        return self.climbStairs(n-1)+self.climbStairs(n-2)
-        
+        if n in self.memo:
+            return self.memo[n]
+        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memo[n]
