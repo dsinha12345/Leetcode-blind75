30e50977ba731a38cbdb7c112b4f3ab2d26f00f5
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-07-05 14:23:22 -0500

    Create contains_duplicate.py

---
+++ contains_duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # hash = {}
+        # for i,enum in enumerate(nums):
+        #     if enum in hash:
+        #         return True
+        #     hash[enum] = i
+        # return False
+        nums_set = set(nums)
+        if len(nums_set)!=len(nums):
+            return True
+        return False
