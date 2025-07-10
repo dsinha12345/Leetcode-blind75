77e13bcbaab52073a6e41737cdd043911bd2fd4b
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-07-10 10:28:17 -0500

    Create is_anagram.py

---
+++ is_anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash_s = {}
+        hash_t = {}
+        for i in s:
+            if i not in hash_s:
+                hash_s[i] = 1
+            else:
+                hash_s[i] = hash_s[i]+1
+        for j in t:
+            if j not in hash_t:
+                hash_t[j] = 1
+            else:
+                hash_t[j] = hash_t[j]+1
+        if hash_s == hash_t:
+            return True
+        else:
+            return False
+        
+        
