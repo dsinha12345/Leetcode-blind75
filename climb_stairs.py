795f40440573d79546f71de833a163c56054c5c3
Devansh Sinha <116433134+dsinha12345@users.noreply.github.com>
2025-09-07 17:08:56 -0500

    Update climb_stairs.py

--- climb_stairs.py
+++ climb_stairs.py
@@ -5,4 +5,5 @@ class Solution:
         if n in self.memo:
             return self.memo[n]
         self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
         return self.memo[n]
